17b08ed73d1adfa93946c6f7f138f65d6abbb3e6
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-11-17 10:48:43 -0300

    task 10 ok

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,16 @@
+from tech_news.database import db
+from collections import Counter
+
+
 # Requisito 10
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    news_categories = [news["category"] for news in db.news.find()]
+    count_categories = Counter(news_categories)
+
+    top_categories = sorted(
+        count_categories.items(),
+        key=lambda x: (-x[1], x[0]))[:5]
+
+    top_5_categories = [category for category, _ in top_categories]
+
+    return top_5_categories
