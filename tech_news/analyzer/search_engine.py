239520bb1d83843ced89f39e57cafac62db77684
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-11-17 09:23:49 -0300

    task 08 ok

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,5 @@
 from tech_news.database import search_news
+from datetime import datetime
 
 
 # Requisito 7
@@ -9,8 +10,16 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        iso_date = datetime.strptime(date, "%Y-%m-%d").strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    query = {"timestamp": iso_date}
+    result_search_news = search_news(query)
+
+    news = [(new["title"], new["url"]) for new in result_search_news]
+    return news
 
 
 # Requisito 9
