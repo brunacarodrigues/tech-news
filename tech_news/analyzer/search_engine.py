0317c1dc44ef1729f1fcaa7a1cbaba065a8a4162
Bruna Rodrigues <brunacris.rodrigues@gmail.com>
2023-11-17 10:18:28 -0300

    task 09 ok

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -24,5 +24,5 @@ def search_by_date(date):
 
 # Requisito 9
 def search_by_category(category):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    query = {"category": {"$regex": category, "$options": "i"}}
+    return [(new["title"], new["url"]) for new in search_news(query)]
